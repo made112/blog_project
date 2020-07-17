c6033c7bd2245c44a693857731f6fa2377c8b2b0
unknown <mmsh7398@gmil.com>
2020-07-17 19:19:50 +0300

    first commit

---
+++ readme.md
@@ -0,0 +1,9 @@
+
+git by mohammed hassan 
+
+
+to install the code 
+composer install
+php artisan migrate
+cb.env.example.env
+php artisan migrate  
---
+++ resources/views/frontend/post.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+
+            <div class="col-md-12">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        {{ $post->title }} - <small>by {{ $post->user->name }}</small>
+
+                        <span class="pull-right">
+                            {{ $post->created_at->toDayDateTimeString() }}
+                        </span>
+                    </div>
+
+                    <div class="panel-body">
+                        <p>{{ $post->body }}</p>
+                        <p>
+                            Category: <span class="label label-success">{{ $post->category->name }}</span> <br>
+                            Tags:
+                            @forelse ($post->tags as $tag)
+                                <span class="label label-default">{{ $tag->name }}</span>
+                            @empty
+                                <span class="label label-danger">No tag found.</span>
+                            @endforelse
+                        </p>
+                    </div>
+                    <div class="card-body">
+                        <p>{{ substr($post->description, 0, 100) }}</p>
+                        <a href="{{ route('post.show', $post->slug) }}"></a>
+                    </div>
+                </div>
+
+                @includeWhen(Auth::user(), 'frontend._form')
+
+                @include('frontend._comments')
+
+            </div>
+
+        </dev>
+    </dev>
+@endsection
